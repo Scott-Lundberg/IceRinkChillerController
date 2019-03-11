9ace00f8980549c5ff46103c2f352b6c06f9df0e
Scott Lundberg <scott.b.lundberg@gmail.com>
2019-03-11 01:29:11 +0000

    modified:   _Globals.py

--- _Globals.py
+++ _Globals.py
@@ -5,7 +5,7 @@ class Globals(object):
     _DBInterface = 'localhost'
     _DBPort = 27017
     _Database = 'IceRinkController'
-    _MQTTBroker = '192.168.112.113'
+    _MQTTBroker = '192.168.112.110'
     _MQTTPort = 1883
     _MQTTLogLevel = 16
     _MessageLog = 'MQTT'
